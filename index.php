542c13b795acb4ab0bf2007f7f3502827266a4c6
webdevmohit <73173798+webdevmohit@users.noreply.github.com>
2021-01-21 15:22:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,226 @@
+<?php require('../resources/config.php'); ?>
+
+<?php include_once(TEMPLATE_FRONT . DS . 'header.php'); ?>
+	<div class="main-wrapper">
+		<?php include_once(TEMPLATE_FRONT . DS . "carousel.php"); ?>
+		<div class="main-cover">
+			
+		<div class="buy-sell-wrapper">
+			<div class="container buy-container">
+				<div class="row buy-row">
+					<div class="offset-lg-7 col-lg-5 col-md-6 col-sm-6 col-7 buy-col">
+						<div class="buy-inner">
+							<div class="page-title">
+								<h1 class="text-white">
+									buy & sell products 
+									<br>
+									with Confidence.
+								</h1>
+							</div>
+							<div class="real-wrap">
+								<img src="img/meter.png" alt="" class="img-fluid meter">
+								<a href="#">
+									Real-time proof of funds
+								</a>
+							</div>
+							<p class="mt-3 text-white">
+								Yubi Global provides real-time Proof of Funds to suppliers to qualify their buyers before negotiating the terms of purchase.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="protect-wrap mt-5">
+			<div class="container protect-container">
+				<div class="row protect-row">
+					<div class="offset-lg-3 col-lg-8  protect-col">
+						<h1 class="text-g">
+							we protect both trading partner
+						</h1>
+						<p>
+							Buyers and Sellers negotiate on the terms and conditions of payment release before the buyer escrow funds. Funds are only released once the agreed upon terms are satisfied.
+						</p>
+					</div>
+				</div>
+				<div class="row section-entry">
+					<div class="protect-icon-wrap">
+						<div class="col mb-4">
+							<div class="pro-icon-img">
+								<img src="img/term-icon.png" alt="" class="img-fluid protect-icon">
+							</div>
+							<h6 class="icon-para">
+								both parties agree on terms
+							</h6>
+						</div>
+						<div class="col mb-4">
+							<div class="pro-icon-img">
+								<img src="img/funds-icon.png" alt="" class="img-fluid protect-icon">
+							</div>
+							<h6 class="icon-para">
+								buyer escrows funds 
+							</h6>
+						</div>
+						<div class="col mb-4">
+							<div class="pro-icon-img">
+								<img src="img/deliver-icon.png" alt="" class="img-fluid protect-icon">
+							</div>
+							<h6 class="icon-para">
+								Seller delivers the product
+							</h6>
+						</div>
+						<div class="col mb-4">
+							<div class="pro-icon-img">
+								<img src="img/approve-icon.png" alt="" class="img-fluid protect-icon">
+							</div>
+							<h6 class="icon-para">
+								Buyer approves release of funds
+							</h6>
+						</div>
+						<div class="col mb-4">
+							<div class="pro-icon-img">
+								<img src="img/lock-icon.png" alt="" class="img-fluid protect-icon">
+							</div>
+							<h6 class="icon-para">
+								We pay the seller
+							</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="hm-market-wrap">
+			<div class="container hm-marekt-container">
+				<div class="row hm-market-row">
+					<div class="col-lg-2  brand-col">
+						<div class="brand-inner">
+							<a href="#">
+								<img src="img/woo-com.png" alt="" class="img-fluid brand">
+							</a>
+							<a href="#">	
+								<img src="img/shopify.png" alt="" class="img-fluid brand">
+							</a>
+							<a href="#">	
+								<img src="img/presto-shop.png" alt="" class="img-fluid brand">
+							</a>	
+							<a href="#">
+								<img src="img/magento.png" alt="" class="img-fluid brand">
+							</a>
+							<a href="#">	
+								<img src="img/opencart.png" alt="" class="img-fluid brand">
+							</a>	
+						</div>
+					</div>
+
+					<div class="offset-lg-6 col-lg-4  market-col">
+						<div class="market-inner">
+							<h4 class="text-b">
+								marketplace
+								<br>
+								integration
+								<br>
+								technology
+							</h4>
+							<p>
+								Easily integrate the Yubi Global 
+								<br>
+								B2B
+								<br>
+								payments and escrow solutions 
+								<br>
+								into
+								<br>
+								your online marketplace
+							</p>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="cash-banking">
+			<div class="container cash-container">
+				<div class="row cash-row align-c">
+					<div class="col-md-5 cash-img-col">
+						<div class="cash-img-inner">
+							<img src="img/cash-view.png" alt="" class="img-fluid cash-im">
+						</div>
+					</div>
+					<div class="col-md-7 cash-text-col">
+						<div class="cash-text-inner">
+							<h1 class="text-white">
+								cash intensive banking
+							</h1>
+							<p class="text-white">
+								Cash intensive business require more due deligence and compliance than traditional businesses. Major correspondent banks are deriking and are moving away from supporting cash intensive businesses. Yubi Global has established relationship with regional banks interested in serving the market
+							</p>
+							<div class="cash-btn-wrap">
+								<a href="#" class="btn c-btn">
+									Creat Your Account
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="cash-manegment m-5">
+			<div class="container cash-manage-container">
+				<div class="row cash-manage-row align-c">
+					<div class="col-md-12 title-col">
+						<h1 class="text-g text-center mb-5">
+							cash management
+						</h1>
+					</div>
+					<div class="col-md-6 cash-manage-col">
+						<div class="cash-manage-inner">
+							<img src="img/management-icon.png" alt="" class="img-fluid cash-manage">
+						</div>
+					</div>
+					<div class="col-md-6 managment-icon-col">
+						<div class="manage-icon-inner">
+							<div class="m-icon-wrap">
+								<img src="img/pickup-icon.png" alt="" class="img-fluid m-icon">
+								<h6>
+									Armored Cash Pickups
+								</h6>
+							</div>
+							<div class="m-icon-wrap">
+								<img src="img/deposit-icon.png" alt="" class="img-fluid m-icon">
+								<h6>
+									FDIC Bank Deposits 
+								</h6>	
+							</div>
+							<div class="m-icon-wrap">
+								<img src="img/funnds.png" alt="" class="img-fluid m-icon">
+								<h6>
+									Immediate Funds Access
+								</h6>	
+							</div>
+							<div class="m-icon-wrap">
+								<img src="img/bil-pay.png" alt="" class="img-fluid m-icon">
+								<h6>
+									Bill Payments
+								</h6>	
+							</div>
+							<div class="m-icon-wrap">
+								<img src="img/purhase.png" alt="" class="img-fluid m-icon">
+								<h6>
+									Escrow Purchases
+								</h6>	
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		</div> <!--  .main-cover -->		
+	</div> <!--  .main-wrapper -->
+
+<?php include_once(TEMPLATE_FRONT . DS . 'footer.php'); ?>
+
